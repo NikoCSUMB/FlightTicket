34afdfde7d296a47fa42f9f720c2fd2bc43febbd
Niko <70172259+NikoCSUMB@users.noreply.github.com>
2021-09-13 17:08:14 -0700

    Static Validation Methods
    CreateActivity has been un-launchable directly for a while now, so the validation methods need to be static in order for the JUnit tests to still work

--- app/src/main/java/com/example/flightticket/CreateActivity.java
+++ app/src/main/java/com/example/flightticket/CreateActivity.java
@@ -100,19 +100,14 @@ public class CreateActivity extends AppCompatActivity {
     /**
      * Searches the list to see if the current username already exists.
      */
-    public boolean validateUser(String username, List<String> names){
-        for (String name : names){
-            if (username.equals(name)){
-                return false;
-            }
-        }
-        return true;
+    public static boolean validateUser(String username, List<String> names){
+        return (!names.contains(username));
     }
 
     /**
      * Checks if both input fields are filled
      */
-    public boolean validateEmpty(String username, String password) {
+    public static boolean validateEmpty(String username, String password) {
         return username.length() != 0 && password.length() != 0;
     }
 
--- app/src/test/java/com/example/flightticket/CreateAccountTest.java
+++ app/src/test/java/com/example/flightticket/CreateAccountTest.java
@@ -11,21 +11,18 @@ import java.util.List;
 public class CreateAccountTest {
     @Test
     public void validateUsernameTest(){
-        CreateActivity activity = new CreateActivity();
         List<String> usernames = new ArrayList<>();
         usernames.add("Test");
 
         // Existing usernames should be rejected
-        assertTrue(activity.validateUser("Yest", usernames));
-        assertFalse(activity.validateUser("Test" ,usernames));
+        assertTrue(CreateActivity.validateUser("Yest", usernames));
+        assertFalse(CreateActivity.validateUser("Test" ,usernames));
     }
 
     @Test
     public void validateEmptyTest(){
-        CreateActivity activity = new CreateActivity();
-
         // Empty fields should be rejected
-        assertFalse(activity.validateEmpty("Username", ""));
-        assertFalse(activity.validateEmpty("", "password"));
+        assertFalse(CreateActivity.validateEmpty("Username", ""));
+        assertFalse(CreateActivity.validateEmpty("", "password"));
     }
 }
