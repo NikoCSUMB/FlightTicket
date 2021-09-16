31df0f4cc6a418bf7e94cee4ffff2a2b95f8cb51
Nicolas Lara Fonseca <53790517+larafonse@users.noreply.github.com>
2021-09-16 11:58:36 -0600

    Update FactoryIntent.java

--- app/src/main/java/com/example/flightticket/utils/FactoryIntent.java
+++ app/src/main/java/com/example/flightticket/utils/FactoryIntent.java
@@ -3,10 +3,26 @@ package com.example.flightticket.utils;
 import android.content.Context;
 import android.content.Intent;
 
+import com.example.flightticket.HomeActivity;
+import com.example.flightticket.LoginActivity;
+
+import java.util.HashMap;
+
 public class FactoryIntent {
     public static Intent getIntent(Class<?> className, Context context) {
+        HashMap<Class<?>, Intent> factoryHash =new HashMap<>();
 
+        if(factoryHash.containsKey(className)==false){
+            factoryHash.put(className,createIntent(className,context));
+        }
+        return (factoryHash.get(className));
+    }
+
+    public static Intent createIntent(Class<?> className, Context context){
         Intent intent = new Intent(context, className);
+
         return (intent);
     }
+
+
 }
