eb937951d2147467b585625a860940f70acaed8a
BorisMarin8004 <bmar8004@gmail.com>
2021-09-17 19:01:54 -0700

    Resolve conflicts.

---
+++ app/src/main/java/com/example/flightticket/FlightListAdapter.java
@@ -0,0 +1,72 @@
+package com.example.flightticket;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.flightticket.DataClasses.Flight;
+import com.example.flightticket.DataClasses.User;
+
+import java.util.List;
+
+public class FlightListAdapter extends RecyclerView.Adapter<FlightListAdapter.MyViewHolder> {
+
+    private Context context;
+    private List<Flight> flightList;
+
+    public FlightListAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void setFlightList(List<Flight> flightList) {
+        this.flightList = flightList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public FlightListAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_row, parent, false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FlightListAdapter.MyViewHolder holder, int position) {
+        holder.flightId.setText(this.flightList.get(position).getId());
+        holder.minPrice.setText(this.flightList.get(position).getMinPrice());
+        holder.flightCarrier.setText(this.flightList.get(position).getCarrier());
+        holder.placeDep.setText(this.flightList.get(position).getPlaceDep().getAirPortName());
+        holder.placeDist.setText(this.flightList.get(position).getPlaceDist().getAirPortName());
+        holder.currency.setText(this.flightList.get(position).getCurrency());
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.flightList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView flightId;
+        TextView minPrice;
+        TextView flightCarrier;
+        TextView placeDep;
+        TextView placeDist;
+        TextView currency;
+
+        public MyViewHolder(View view) {
+            super(view);
+            flightId = view.findViewById(R.id.flightId);
+            minPrice = view.findViewById(R.id.minPrice);
+            flightCarrier = view.findViewById(R.id.flightCarrier);
+            placeDep = view.findViewById(R.id.placeDep);
+            placeDist = view.findViewById(R.id.placeDist);
+            currency = view.findViewById(R.id.currency);
+        }
+    }
+}
--- app/src/main/java/com/example/flightticket/SavedFlights.java
+++ app/src/main/java/com/example/flightticket/SavedFlights.java
@@ -1,4 +1,68 @@
 package com.example.flightticket;
 
-public class SavedFlights {
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
+
+import com.example.flightticket.DB.UserDAO;
+import com.example.flightticket.DB.UserDatabase;
+import com.example.flightticket.DataClasses.Flight;
+
+import java.util.List;
+
+public class SavedFlights extends AppCompatActivity {
+    UserDAO userDAO;
+    private FlightListAdapter flightListAdapter;
+    private List<Flight> flights;
+
+    SharedPreferences userPref;
+    SharedPreferences.Editor userPrefEditor;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_flights);
+
+        TextView welcomeText = findViewById(R.id.userWelcome);
+
+        userPref = getSharedPreferences("userPreferences", MODE_PRIVATE);
+        String welcome = "Welcome, " + userPref.getString("username", "");
+        welcomeText.setText(welcome);
+
+        String username = userPref.getString("username", "");
+
+        initRecyclerView();
+        loadFlightList(username);
+
+    }
+
+
+    private void initRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        flightListAdapter = new FlightListAdapter(this);
+        recyclerView.setAdapter(flightListAdapter);
+    }
+
+    private void loadFlightList(String username) {
+        userDAO = Room.databaseBuilder(this, UserDatabase.class, UserDatabase.DB_NAME)
+                .allowMainThreadQueries()
+                .fallbackToDestructiveMigration()
+                .build().getUserDAO();
+        flights = userDAO.getUserByUsername(username).getFlights();
+        flightListAdapter.setFlightList(flights);
+    }
 }
+
+
+
