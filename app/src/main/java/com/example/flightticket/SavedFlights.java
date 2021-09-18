69d41afb4868ad36a5fcc773b4f00bf89eedd64e
BorisMarin8004 <bmar8004@gmail.com>
2021-09-17 19:04:12 -0700

    Resolve conflicts.

--- app/src/main/java/com/example/flightticket/SavedFlights.java
+++ app/src/main/java/com/example/flightticket/SavedFlights.java
@@ -2,65 +2,48 @@ package com.example.flightticket;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
 import com.example.flightticket.DB.UserDAO;
 import com.example.flightticket.DB.UserDatabase;
 import com.example.flightticket.DataClasses.Flight;
+import com.example.flightticket.DataClasses.User;
 
 import java.util.List;
 
 public class SavedFlights extends AppCompatActivity {
-    UserDAO userDAO;
-    private FlightListAdapter flightListAdapter;
-    private List<Flight> flights;
+    private TextView title;
+    private ListView flightsListView;
+    private Button filterFlightsBtn;
 
-    SharedPreferences userPref;
-    SharedPreferences.Editor userPrefEditor;
+    private List<Flight> savedFlights;
 
+    private UserDAO userDAO;
+    private User user;
+    private SharedPreferences userPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_flights);
 
-        TextView welcomeText = findViewById(R.id.userWelcome);
+        title = findViewById(R.id.TitleView);
+        flightsListView = findViewById(R.id.SavedFlightsView);
+        filterFlightsBtn = findViewById(R.id.FilterFlightsBtn);
 
-        userPref = getSharedPreferences("userPreferences", MODE_PRIVATE);
-        String welcome = "Welcome, " + userPref.getString("username", "");
-        welcomeText.setText(welcome);
-
-        String username = userPref.getString("username", "");
-
-        initRecyclerView();
-        loadFlightList(username);
-
-    }
-
-
-    private void initRecyclerView() {
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
-        recyclerView.addItemDecoration(dividerItemDecoration);
-        flightListAdapter = new FlightListAdapter(this);
-        recyclerView.setAdapter(flightListAdapter);
-    }
-
-    private void loadFlightList(String username) {
         userDAO = Room.databaseBuilder(this, UserDatabase.class, UserDatabase.DB_NAME)
                 .allowMainThreadQueries()
                 .fallbackToDestructiveMigration()
                 .build().getUserDAO();
-        flights = userDAO.getUserByUsername(username).getFlights();
-        flightListAdapter.setFlightList(flights);
+
+        userPref = getSharedPreferences("userPreferences", MODE_PRIVATE);
+        user = userDAO.getUserByUsername(userPref.getString("username", ""));
+
     }
 }
 
